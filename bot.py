553835bb848d66223cd4470f0296629f4174c0df
tgyhjudwqgdhyujqwdgyhuwjikqd <inkgamenation@gmail.com>
2026-07-20 05:05:10 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,25 @@
 import discord
-from discord.ext import commands
 import aiohttp
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-INVITE_LINK = "https://discord.gg/hsx"
 AVATAR_URL = "https://cdn.discordapp.com/attachments/1513695339167617084/1528479464034537532/6474.webp?ex=6a5e72ee&is=6a5d216e&hm=4a8ee3d3694f16aba6398c57d8098656aa6213a63c9a919e75e141d2536d0660&"
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='.', intents=intents)
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        activity = discord.Game(name="HollyScriptX")
+        await self.change_presence(activity=activity)
+        
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(AVATAR_URL) as resp:
+                    if resp.status == 200:
+                        image_data = await resp.read()
+                        await self.user.edit(avatar=image_data)
+                        print('Avatar updated')
+        except Exception as e:
+            print(f'Avatar error: {e}')
 
-class InviteView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        button = discord.ui.Button(label="Join Discord", url=INVITE_LINK)
-        self.add_item(button)
-
-@bot.event
-async def on_ready():
-    print(f'Bot {bot.user} is online')
-    activity = discord.Game(name="HollyScriptX")
-    await bot.change_presence(activity=activity)
-    
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(AVATAR_URL) as resp:
-                if resp.status == 200:
-                    image_data = await resp.read()
-                    await bot.user.edit(avatar=image_data)
-                    print('Avatar updated')
-    except Exception as e:
-        print(f'Avatar error: {e}')
-
-@bot.command()
-async def invite(ctx):
-    view = InviteView()
-    await ctx.send("Join our discord!", view=view)
-
-@bot.command()
-async def status(ctx):
-    embed = discord.Embed(
-        title="HollyScriptX",
-        description="discord.gg/hsx",
-        color=discord.Color.blue()
-    )
-    embed.set_image(url=AVATAR_URL)
-    view = InviteView()
-    await ctx.send(embed=embed, view=view)
-
-if __name__ == "__main__":
-    if TOKEN is None:
-        print("ERROR: DISCORD_TOKEN not set!")
-    else:
-        bot.run(TOKEN)
+client = MyClient(intents=discord.Intents.all())
+client.run(TOKEN)
